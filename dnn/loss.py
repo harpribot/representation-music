8651174abdd1974a24d508847144f92a63d14410
harpribot <harshalpriyadarshi6@gmail.com>
2016-11-17 17:54:29 -0600

    adds placeholder for loss, and updates regularization layer

---
+++ dnn/loss.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+
+
+def mse():
+    pass
--- dnn/regularization.py
+++ dnn/regularization.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 
 
-def dropout_layer(input_layer, dropout_ratio):
+def dropout_layer(input_layer, dropout_ratio=0.5):
     return tf.nn.dropout(input_layer, dropout_ratio)
 
 
-def batch_norm_layer(input_layer, weights, output_dim, epsilon):
+def batch_norm_layer(input_layer, weights, epsilon):
     z = tf.matmul(input_layer, weights)
+    output_dim = weights.get_shape()[2].value
     batch_mean, batch_var = tf.nn.moments(z, [0])
     scale = tf.Variable(tf.ones([output_dim]))
     beta = tf.Variable(tf.ones([output_dim]))
