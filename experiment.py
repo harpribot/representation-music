cc5c60d58c6ed5211b0eef88e83dee8a72ab8ebe
harpribot <harshalpriyadarshi6@gmail.com>
2016-11-18 00:59:37 -0600

    Tester script for a simple experiment.

--- experiment.py
+++ experiment.py
@@ -2,25 +2,50 @@ import tensorflow as tf
 from Models.models import LowestSharingModel
 from dnn.optimizer import Optimizer
 import numpy as np
+from dnn.loss import mse
+
+LEARNING_RATE = 1e-4
 
 
 class Experiment(LowestSharingModel):
-    def __init__(self, input_info, output_info):
+    def __init__(self, input_info, output_info, input_data, output_labels):
         LowestSharingModel.__init__(self, input_info, output_info)
         self.sess = None
+        self.input_data = input_data
+        self.output_labels = output_labels
 
     def initialize_network(self):
         self.sess = tf.InteractiveSession()
         self._create_model()
+        self.__initialize_trainer()
+        self.sess.run(tf.initialize_all_variables())
+
+    def __initialize_trainer(self):
+        self.cost = mse(0., 0.)
+        for output_id, _ in self.output_info:
+            self.cost += self.layers['loss-' + output_id]
+
+        opt = Optimizer(self.cost)
+        self.optimizer = opt.get_adagrad(LEARNING_RATE)
 
     def train(self):
-        pass
+        feed_dict = dict()
+        feed_dict[self.layers['input']] = [self.input_data]
+        for output_id, _ in self.output_info:
+            feed_dict[self.layers['ground-truth-' + output_id]] = [[self.output_labels[output_id]]]
+
+        self.sess.run(self.optimizer, feed_dict=feed_dict)
+
+        # see both of the outputs
+        print self.sess.run([self.layers['output-' + '1'], self.layers['output-' + '2']], feed_dict=feed_dict)
 
 
-input = np.random.rand(5000)
+input_data = np.random.rand(5000)
+labels = {'1':0.5, '2':0.8}
 input_info = ('input', 5000)
-output_info = [('output-1', 1), ('output-2', 1)]
-exp = Experiment(input_info, output_info)
+output_info = [('1', 1), ('2', 1)]
+exp = Experiment(input_info, output_info, input_data, labels)
 exp.initialize_network()
+exp.train()
 
 
