d1f00236a87f0a8b2c4439a64e939d31bb1b5c05
harpribot <harshalpriyadarshi6@gmail.com>
2016-11-17 20:52:32 -0600

    Tester script to see if your models are good

--- experiment.py
+++ experiment.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from Models.models import LowestSharingModel
+import
 
 
 class Experiment(LowestSharingModel):
@@ -10,5 +11,12 @@ class Experiment(LowestSharingModel):
     def initialize_network(self):
         self.sess = tf.InteractiveSession()
         self._create_model()
+
+
+input =
+input_info = ('input', 5000)
+output_info = [('output-1', 1), ('output-2', 1)]
+exp = Experiment(input_info, output_info)
+exp.initialize_network()
 
 
