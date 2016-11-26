317fceca36a3a1db5df90f461dde0705d8f2e21e
harpribot <harshal.priyadarshi@utexas.edu>
2016-11-26 16:00:45 -0600

    Fixes tensorflow import error: Tensorflow import should always be at top

--- experiment.py
+++ experiment.py
@@ -1,8 +1,8 @@
+import tensorflow as tf
 import os
 import time
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
 from dnn.loss import mse
 from dnn.optimizer import Optimizer
 from Models.low_level_sharing_model import LowLevelSharingModel
