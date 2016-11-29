6d8bd42a0ac64620b33d54427e264933da5cd904
harpribot <harshalpriyadarshi6@gmail.com>
2016-11-28 22:47:25 -0600

    Correcting sbatch scripts

--- multi-task-low.sh
+++ multi-task-low.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-SBATCH -J MultiTaskLow                # job name
-SBATCH -o multi_low_log.o%j               # output and error file name (%j expands to jobID)
-SBATCH -n 1                           # total number of gpu nodes requested
-SBATCH -p gpu                         # queue (partition) -- normal, development, etc.
-SBATCH -t 10:00:00                    # run time (hh:mm:ss) - 12 hours
-SBATCH --mail-user=tyler@cs.utexas.edu
-SBATCH --mail-type=begin              # email me when the job starts
-SBATCH --mail-type=end                # email me when the job finishes
+#SBATCH -J MultiTaskLow                # job name
+#SBATCH -o multi_low_log.o%j               # output and error file name (%j expands to jobID)
+#SBATCH -n 1                           # total number of gpu nodes requested
+#SBATCH -p gpu                         # queue (partition) -- normal, development, etc.
+#SBATCH -t 10:00:00                    # run time (hh:mm:ss) - 12 hours
+#SBATCH --mail-user=tyler@cs.utexas.edu
+#SBATCH --mail-type=begin              # email me when the job starts
+#SBATCH --mail-type=end                # email me when the job finishes
 
 python ../multi-task-low.py
