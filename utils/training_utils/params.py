6797c9b705b0cfd5c770f68695402baf044510ff
Shobhit Chaurasia <sc.shobhit@gmail.com>
2016-11-28 18:40:25 -0600

    Change default value of checkpoint frequency
    The default values of evaluation and checkpoint frequency are the same,
    and they are approximately equal to the number of steps in each epoch.

--- utils/training_utils/params.py
+++ utils/training_utils/params.py
@@ -6,8 +6,8 @@ TOTAL_NUM_EXAMPLES = 147052     # Total number of tracks
 assert(TRAIN_FRACTION + VALIDATE_FRACTION + TEST_FRACTION == 1.0)
 
 # Validation and check pointing
-EVALUATION_FREQ = 1000   # Number of mini-batch SGD steps after which an evaluation is performed.
-CHECKPOINT_FREQ = 1000   # Number of mini-batch SGD steps after which a checkpoint is taken.
+EVALUATION_FREQ = 900   # Number of mini-batch SGD steps after which an evaluation is performed.
+CHECKPOINT_FREQ = 900   # Number of mini-batch SGD steps after which a checkpoint is taken.
 
 # File paths
 EXPT_DIRECTORY_PATH = "./Experiments"  # Path of the Experiments directory
